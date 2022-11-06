2c29638bab4d20f70dc1a7abd76b3eefeaac9a35
jakeline-freitas <jakefreaitas@gmail.com>
2022-11-06 10:32:49 -0300

    :sparkles: profile route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -3,8 +3,14 @@ import { z } from "zod"
 import { prisma } from "../lib/prisma"
 
 import fetch from "node-fetch"
+import { authenticate } from "../plugins/authenticate"
 
 export async function authRoutes(fastify: FastifyInstance){
+    fastify.get("/me", {
+            onRequest: [authenticate]
+        }, async (request) => {
+        return { user: request.user }
+    })
 
     fastify.post("/users", async (request) => {
         const createUserBody = z.object({
